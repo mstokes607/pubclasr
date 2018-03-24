34b1b6a845706be2e76605b517c692469d8252dc
Michael Stokes <me_stokes@yahoo.com>
2018-03-24 14:54:23 -0400

    Add files via upload

--- pubclasr/main.py
+++ pubclasr/main.py
@@ -8,12 +8,12 @@ import networkx as nx
 import matplotlib.pyplot as plt
 
 # process the raw text abstract file -> dataframe of abstract texts, titles, and journal info is returned
-filename = '~/pubclasr/data/pubmed_result.xml'
+filename = '/Users/nathmorissette/miniconda2/envs/pubclasr/pubclasr/pubmed_result.xml'
 data = process.processXml(filename)
 
 # id abstracts with pharma companies in abstract affiliations
-pharma_lst = ['pfizer','novartis','roche ','sanofi','merck','gilead','johnson and johnson','glaxo', 'takeda', 'astrazeneca',
-              'bristol-myers']
+pharma_lst = ['pfizer','novartis','roche ','sanofi','merck','gilead','johnson and johnson', 'johnson & johnson', 'glaxo', 
+              'gsk', 'takeda', 'astrazeneca', 'bristol-myers']
               
 pharma = process.id_abstracts(data, pharma_lst, anyflag=1)
 subset = pharma[pharma['any']==1]
@@ -21,14 +21,22 @@ print 'pharma pubs'
 print subset[pharma_lst].sum()
 print '\n'
 
-co_lst = ['evidera']      
-evi = process.id_abstracts(subset, co_lst, anyflag=0)
-evi = evi[evi['evidera']==1]
-print 'subset pharma pubs with evidera'
-print evi[pharma_lst].sum()
-print '\n'
+# check to see if pharma affiliations were appropriately identified in the abstract data
+# pharma['random'] = np.random.rand(len(pharma))
+# pharma = pharma.sort_values(by=['random'])
+# pharma.to_csv('/Users/nathmorissette/miniconda2/envs/pubclasr/pubclasr/processed.csv', encoding='utf-8')
 
-evi_data = evi[pharma_lst].sum()
+# clean up where two vars referenced the same organization
+condition = (subset['glaxo'] == 1) | (subset['gsk'] == 1)
+subset['glaxosmithkline'] = np.where(condition, 1, 0) 
+condition2 = (subset['johnson and johnson'] == 1) | (subset['johnson & johnson'])
+subset['j&j'] = np.where(condition2, 1, 0) 
+subset = subset.drop(['glaxo','gsk', 'johnson and johnson', 'johnson & johnson'], axis=1)
+pharma_lst = [e for e in pharma_lst if e not in ('gsk', 'glaxo', 'johnson and johnson', 'johnson & johnson')]
+pharma_lst.append('glaxosmithkline')
+pharma_lst.append('j&j')
+
+evi_data = subset[pharma_lst].sum()
 evi_edges = graph_util.create_edges('evi', evi_data)
 
 # Create the Graph data - circular graph with consulting company at the center
@@ -49,6 +57,8 @@ def create_graph(node_data, edge_data):
     edge_labels=dict([((u,v,),d['weight'])
              for u,v,d in G.edges(data=True)])
     nx.draw_networkx_edge_labels(G,pos,edge_labels=edge_labels, font_size=8)
+    #plt.savefig(output, format='PNG')
     plt.show()
-    
-create_graph(list(evi_data.index), evi_edges)
+
+output = '/Users/nathmorissette/miniconda2/envs/pubclasr/pubclasr/evi_graph.png'
+create_graph(evi_data.index.values, evi_edges)
